30c882abae54b77072b90b3386716d027379efcd
Ali Reubenstone <a.stone1794@gmail.com>
2019-08-05 11:12:45 -0400

    Center social icons and add google font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
+import { Icon } from 'semantic-ui-react'
 // import '../App.css';
 
 
 const Header = () => {
   return (
-    <div id="header">
-      <h1 id="myName">
+    <div class="header">
+      <h1 id="my-name">
         ALI REUBENSTONE
       </h1>
-      <div id="ReactTypingEffect">
+      <div id="react-typing-effect">
         <ReactTypingEffect
-          text={["Full Stack Software Developer", "Former Actor", "Finance Grad", "Emory Alum", "Improv Lover"]}
-          eraseDelay={200}
+          text={["Full Stack Web Developer", "Former Actor", "Finance Grad", "Emory Alum", "Improv Lover"]}
+          eraseDelay={100}
           speed={100}
         />
       </div>
-      <h4 id="resume">
-        <a href="https://drive.google.com/file/d/1gDm3SgYmzSwBVx8oMKHOHb0bHmZWYBuV/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-          View My Resume
-        </a>
-      </h4>
-    </div>
+        <h4 id="resume">
+          <a href="https://drive.google.com/file/d/1gDm3SgYmzSwBVx8oMKHOHb0bHmZWYBuV/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            View My Resume
+          </a>
+        </h4>
+        <span class="social-icons">
+          <a href="https://www.linkedin.com/in/alireubenstone/" target="_blank" rel="noopener noreferrer">
+            <Icon name="linkedin" size='big' aria-hidden="true" />
+          </a>
+          <a href="https://github.com/Rexman17" target="_blank" rel="noopener noreferrer">
+            <Icon name="github" size='big' aria-hidden="true" />
+          </a>
+          <a href="https://medium.com/@alireubenstone" target="_blank" rel="noopener noreferrer">
+            <Icon name="medium" size='big' aria-hidden="true" />
+          </a>
+        </span>
+      </div>
   )
 }
 
