53c3bde18827b3d0056990bfa9665d265ab3a3e4
Ali Reubenstone <a.stone1794@gmail.com>
2019-08-06 14:41:17 -0400

    Add skills

---
+++ src/components/Skills.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Skills = () => {
+
+
+  return (
+    <div className="skills">
+
+      <div className="s1">
+        <p className="skill-type">
+          Programming Languages
+        </p>
+        <p>JavaScript</p>
+        <p>Ruby</p>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>SQL</p>
+      </div>
+
+      <div className="s2">
+        <p className="skill-type">
+          Libraries & Frameworks
+        </p>
+        <p>React</p>
+        <p>Redux</p>
+        <p>Rails</p>
+      </div>
+
+      <div className="s3">
+        <p className="skill-type">
+          Other Skills
+        </p>
+        <p>Public Speaking</p>
+        <p>Improv Comedy</p>
+        <p>Acting</p>
+        <p>Writing</p>
+      </div>
+
+      <div className="s4">
+        <p className="skill-type">
+          Interests
+        </p>
+        <p>Sketch Comedy</p>
+        <p>Reading</p>
+        <p>Tennis</p>
+        <p>Golf</p>
+        <p>Lacrosse</p>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default Skills
--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProjectsList from '../components/ProjectsList'
 import Blogs from '../components/Blogs'
+import Skills from '../components/Skills'
 // import { Button } from 'semantic-ui-react'
 
 class ProjectsContainer extends React.Component {
@@ -13,7 +14,7 @@ class ProjectsContainer extends React.Component {
         PROJECTS
       </h2>
 
-  
+
         <ProjectsList />
 
 
@@ -21,11 +22,18 @@ class ProjectsContainer extends React.Component {
         <h2 id="blogs-title">
           BLOGS
         </h2>
-<<<<<<< HEAD
+
         <Blogs />
-=======
-        Coming soon...
->>>>>>> 5413675d662a207566f35b1aea9a909cb00ed770
+
+      </div>
+
+      <div id="skills">
+        <h2 id="skills-title">
+          SKILLS
+        </h2>
+
+        <Skills />
+
       </div>
 
     </div>
